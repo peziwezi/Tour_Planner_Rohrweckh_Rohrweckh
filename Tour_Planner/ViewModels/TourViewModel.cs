de2a73f8cf0fafcc2fb23787a4597adef0e4043a
Patrick Rohrweckh <if23b154@technikum-wien.at>
2025-03-24 13:07:15 +0100

    Crud UI

--- Tour_Planner/ViewModels/TourViewModel.cs
+++ Tour_Planner/ViewModels/TourViewModel.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tour_Planner.Models;
 
 namespace Tour_Planner.ViewModels
 {
     class TourViewModel : BaseViewModel
     {
+        public ObservableCollection<Tour> Data { get; } = [];
     }
 }
